6001cda998c5a0fb43c07d4cdddfb0a56e988c45
DESKTOP-SPNOEQI <jjescuderv@eafit.edu.co>
2020-10-23 17:58:59 -0500

    Update COMMAND_MESSAGE

--- constants.py
+++ constants.py
@@ -9,6 +9,7 @@ BACKLOG = 10
 RCV_BUFFER_SIZE = 1024
 PATH = 'C:/Users/Juan Escudero/Desktop/Telematica PR1/server_files'
 
+CHANGE_DIRECTORY = 'CD'
 LIST_ALL = 'ALL'                # Lists everything                         |    No parameters
 CREATE_BUCKET = 'BUCKETC'       # Create a new bucket                      |    bucket name
 DELETE_BUCKET = 'BUCKETD'       # Delete a selected bucket                 |    bucket id
@@ -17,25 +18,26 @@ LIST_FILES = 'FILEL'            # List all files of an existing bucket     |    
 UPLOAD_FILE = 'UPFILE'          # Upload a file to the server              |    file name and extension
 DOWNLOAD_FILE = 'DWFILE'        # Download a file from the server          |    bucket id, file id
 DELETE_FILE = 'DEFILE'          # Delete a file from an existing bucket    |    bucket id, file id
+COMMAND_INFO = 'HELP'
 DISCONNECT_COMMAND = 'QUIT'     # Disconnects from server                  |    No parameters
 
 COMMAND_MESSAGE = {
-    100: 'BUCKET CREATED',
-    101: 'FILE CREATED',
-    200: 'BUCKET DELETED',
-    201: 'FILE DELETED',
-    300: 'DISCONNECTED',
-    400: 'UNKNOWN COMMAND',
-    401: 'TOO MANY ARGUMENTS',
-    402: 'NOT ENOUGH ARGUMENTS',
-    403: 'INVALID ARGUMENTS',
-    404: 'BUCKET NOT FOUND',
-    405: 'FILE NOT FOUND',
-    406: 'FILE ALREADY EXISTS',
-    407: 'BUCKET ALREADY EXISTS',
-    500: 'FILE UPLOADED',
-    501: 'FILE DOWNLOADED',
-    600: 'LIST RETRIEVED',
+    100: '[100] BUCKET CREATED',
+    101: '[101] FILE CREATED',
+    200: '[200] BUCKET DELETED',
+    201: '[201] FILE DELETED',
+    300: '[300] DISCONNECTED',
+    400: '[400] UNKNOWN COMMAND',
+    401: '[401] TOO MANY ARGUMENTS',
+    402: '[402] NOT ENOUGH ARGUMENTS',
+    403: '[403] INVALID ARGUMENTS',
+    404: '[404] BUCKET NOT FOUND',
+    405: '[405] FILE NOT FOUND',
+    406: '[406] FILE ALREADY EXISTS',
+    407: '[407] BUCKET ALREADY EXISTS',
+    500: '[500] FILE UPLOADED',
+    501: '[501] FILE DOWNLOADED',
+    600: '[600] LIST RETRIEVED',
 }
 # Not used yet
 COMMAND_REQUIREMENTS = {  
